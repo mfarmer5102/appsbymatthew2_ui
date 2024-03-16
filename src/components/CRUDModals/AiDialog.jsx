ca094f9b704e4bb237da819bcfed1700367987f6
mfarmer5102 <mfarmer5102@gmail.com>
2024-03-16 00:04:15 -0400

    now able to provide a chat-like experience with ai assistant

--- src/components/CRUDModals/AiDialog.jsx
+++ src/components/CRUDModals/AiDialog.jsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useRef, useState} from 'react';
 import {
     Button,
     Dialog,
@@ -12,35 +12,22 @@ import {
 import {ApplicationContext} from '../../context/ApplicationContext';
 import {processTextWithAi, searchEmbeddingsPlus} from "../../services/ai.service";
 
-const ResponseIndicator = ({aiResponse}) => {
-
-    useEffect(() => {}, [aiResponse])
-
-    // return (
-    //     <TextField
-    //         id="outlined-basic"
-    //         value={aiResponse}
-    //         // label="Prompt"
-    //         variant="outlined"
-    //         multiline={true}
-    //         fullWidth={true}
-    //         disabled={true}
-    //     />
-    // )
-
-    return aiResponse;
-
-}
 
 const AiDialog = (props) => {
     const AppContext = useContext(ApplicationContext);
+    const messagesRef = useRef();
     const [open, setOpen] = React.useState(false);
     const [aiSubmission, setAiSubmission] = useState(null);
-    const [aiResponse, setAiResponse] = useState('Response will go here');
+    const [aiResponse, setAiResponse] = useState('');
+    const [chatLog, setChatLog] = useState([
+        {
+            text: "Hello! Ask me anything about Matt's skills and applications.",
+            role: "system"
+        }
+    ]);
+    const [lastUpdatedChat, setLastUpdatedChat] = useState(new Date())
 
-    useEffect(() => {
-        console.log(props.data);
-    }, []);
+    useEffect(() => {}, []);
 
     const handleClickOpen = () => {
         setOpen(true);
@@ -54,9 +41,14 @@ const AiDialog = (props) => {
         try {
             let params = `text=${aiSubmission}`
             const res = await searchEmbeddingsPlus(params);
-            console.log(res)
-            // setAiResponse(res)
-            setAiResponse(res.text);
+            const updatedChatLog = chatLog;
+            updatedChatLog.push({
+                role: 'system',
+                text: res.text
+            })
+            console.log(updatedChatLog)
+            setChatLog(updatedChatLog);
+            setLastUpdatedChat(new Date());
         } catch (e) {
             console.log(e)
             AppContext.handleError('Unable to process.');
@@ -64,22 +56,49 @@ const AiDialog = (props) => {
     }
 
     const populateModalContents = () => {
+        const messageBubbles = [];
+        chatLog.forEach(item => {
+            if (item.role === 'system') {
+                messageBubbles.push(
+                    <div style={{width: 'fit-content', float: 'left', background: '#0072ff', color: 'white', margin: '10px 100px 10px 0px', padding: '10px', borderRadius: '20px'}}>
+                        {item.text}
+                    </div>
+                )
+            } else if (item.role === 'user') {
+                messageBubbles.push(
+                    <div style={{width: 'fit-content', float: 'right', background: 'silver', color: 'black', margin: '10px 0px 10px 100px', padding: '10px', borderRadius: '20px'}}>
+                        {item.text}
+                    </div>
+                )
+            }
+        });
+
         return (
             <div style={{padding: '20px', minWidth: '500px'}}>
+                {messageBubbles}
                 <TextField
                     id="outlined-basic"
-                    placeholder='Enter prompt here'
+                    placeholder='Enter prompt here, then press Enter.'
                     value={aiSubmission}
                     // label="Outlined"
                     variant="outlined"
-                    multiline={true}
+                    // multiline={true}
                     fullWidth={true}
                     onChange={e => setAiSubmission(e.target.value)}
+                    onKeyDown={(e)=> {
+                        if (e.key === 'Enter') {
+                            submitToAi(e.target.value)
+                            const updatedChatLog = chatLog;
+                            updatedChatLog.push({
+                                role: 'user',
+                                text: aiSubmission
+                            })
+                            setChatLog(updatedChatLog)
+                            setAiSubmission('')
+                        }
+                    }}
                 />
-                <br/><br/>
-                <ResponseIndicator aiResponse={aiResponse}/>
-                <br/>
-                <Button variant='outlined' onClick={submitToAi}>Submit</Button>
+                {/*<Button variant='outlined' onClick={submitToAi}>Submit</Button>*/}
             </div>
         )
     }
