c8e29d3ed6b1733531753404887593eb7347e07e
Matthew Farmer <mfarmer5102@gmail.com>
2025-05-07 17:03:32 -0400

    updating About modal

--- src/components/CRUDModals/Welcome.jsx
+++ src/components/CRUDModals/Welcome.jsx
@@ -30,7 +30,7 @@ const WelcomeDialog = (props) => {
             <DialogTitle className='modal primary-font'>Hello!</DialogTitle>
             <DialogContent className='modal primary-font'>
                 <DialogContentText className='modal primary-font'>
-                    I'm a professional full-stack web developer and data engineer with a passion for developing applications and preparing data to solve real-world problems. In addition to almost 5 years of experience as a professional full-stack developer and data engineer, I possess more than 10 years of professional experience with creative problem solving.
+                    I'm a professional data engineer and full-stack web developer with a passion for developing applications and transforming data to solve real-world problems. In addition to more than 5 years of experience as a professional and data engineer and full-stack developer, I possess more than 10 years of professional experience with creative problem solving.
                 </DialogContentText>
             </DialogContent>
             <DialogActions className='modal primary-font'>
