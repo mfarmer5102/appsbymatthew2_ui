e28525706ebd113f48324702e1ce0b791180218f
Matthew Farmer <mfarmer5102@gmail.com>
2025-05-26 17:19:12 -0400

    adding aws workflow

--- src/services/supportStatus.service.js
+++ src/services/supportStatus.service.js
@@ -11,7 +11,8 @@ export async function getSupportStatuses(params) {
         // body: JSON.stringify({username, password})
     });
     if (response.ok) {
-        return await response.json();
+        const res = await response.json();
+        return res?.body;
     } else {
         throw new Error('Unable to load support statuses.');
     }
